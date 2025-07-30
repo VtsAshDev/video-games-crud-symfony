e1442f1443389af25cb0ce7b39fe26d881742249
VtsAshDev <vitorgguimaraes56@gmail.com>
2025-07-29 22:31:00 -0300

    Criado VideoGameDto para validacao do mapRequest para passar para a Entidade VideoGame escrever no banco de acordo com o metodo

---
+++ src/Dto/VideoGameDto.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Dto;
+use App\Entity\Platform;
+use Symfony\Component\Validator\Constraints as Assert;
+class VideoGameDto
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 1, max: 100)]
+        public readonly string $title,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 1, max: 100)]
+        public readonly string $genre,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 1, max: 100)]
+        public readonly string $developer,
+
+        #[Assert\NotBlank]
+        public readonly \DateTime $releaseDate,
+    ) {
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getGenre(): string
+    {
+        return $this->genre;
+    }
+
+    public function getDeveloper(): string
+    {
+        return $this->developer;
+    }
+
+    public function getReleaseDate(): \DateTime
+    {
+        return $this->releaseDate;
+    }
+}
